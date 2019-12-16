c55eeb7d4f15efc18af1c99a5d80d0f42d090f77
Claudio Bustos <bustos.cl@gmail.com>
2019-12-15 23:21:11 -0300

    Json para llenar article

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,7 +1,54 @@
 $(document).ready(function(){
+
+  //slider
   $('.bxslider').bxSlider({
    mode: 'fade',
    captions: true,
    slideWidth: 1200
  });
+
+  //posts (arreglo de JSON)
+  var posts = [
+         {
+           title: "prueba de titulo 1",
+           date: moment().format("MMMM Do YYYY"),
+           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+         },
+         {
+           title: "prueba de titulo 2",
+          date: moment().format("MMMM Do YYYY"),
+           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+         },
+         {
+           title: "prueba de titulo 3",
+           date: moment().format("MMMM Do YYYY"),
+           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+         },
+         {
+           title: "prueba de titulo 4",
+          date: moment().format("MMMM Do YYYY"),
+           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+         },
+         {
+           title: "prueba de titulo 5",
+           date: moment().format("MMMM Do YYYY"),
+           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+         }
+       ];
+
+       posts.forEach((item, index) => {
+         var post = `
+             <article class="post">
+                 <h2>${item.title}</h2>
+                 <span class="date">${item.date}</span>
+                 <p>${item.content}</p>
+                 <a href="#" class="button-more">Leer mas</a>
+             </article>
+         `;
+
+          $("#posts").append(post);
+
+       });
+
+
 });
