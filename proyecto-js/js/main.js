003b16853d092d20eab9ce3ab41d02b28854aa30
Claudio79 <clabustos@colmena.cl>
2019-12-18 16:33:54 -0300

    reloj y acordeon listos

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,40 +1,43 @@
 $(document).ready(function(){
 
   //slider
-  $('.bxslider').bxSlider({
-   mode: 'fade',
-   captions: true,
-   slideWidth: 1200
- });
-
+  if (window.location.href.indexOf('index') > -1) {
+    $('.bxslider').bxSlider({
+     mode: 'fade',
+     captions: true,
+     slideWidth: 1200
+   });
+   }
   //posts (arreglo de JSON)
-  var posts = [
-         {
-           title: "prueba de titulo 1",
-           date: moment().format("MMMM Do YYYY"),
-           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-         },
-         {
-           title: "prueba de titulo 2",
-          date: moment().format("MMMM Do YYYY"),
-           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-         },
-         {
-           title: "prueba de titulo 3",
-           date: moment().format("MMMM Do YYYY"),
-           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-         },
-         {
-           title: "prueba de titulo 4",
-          date: moment().format("MMMM Do YYYY"),
-           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-         },
-         {
-           title: "prueba de titulo 5",
-           date: moment().format("MMMM Do YYYY"),
-           content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-         }
-       ];
+    if (window.location.href.indexOf('index') > -1) {
+      var posts = [
+             {
+               title: "prueba de titulo 1",
+               date: moment().format("MMMM Do YYYY"),
+               content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+             },
+             {
+               title: "prueba de titulo 2",
+              date: moment().format("MMMM Do YYYY"),
+               content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+             },
+             {
+               title: "prueba de titulo 3",
+               date: moment().format("MMMM Do YYYY"),
+               content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+             },
+             {
+               title: "prueba de titulo 4",
+              date: moment().format("MMMM Do YYYY"),
+               content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+             },
+             {
+               title: "prueba de titulo 5",
+               date: moment().format("MMMM Do YYYY"),
+               content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+             }
+           ];
+
 
        posts.forEach((item, index) => {
          var post = `
@@ -48,7 +51,8 @@ $(document).ready(function(){
 
           $("#posts").append(post);
 
-       });
+          });
+        }
 
        //Selector de tema.
        var tema = $('#tema');
@@ -73,4 +77,41 @@ $(document).ready(function(){
           },1000);
         //  return false;
         });
+
+          //Login falso
+          $('#login form').submit(function(){
+            var formName = $('#name').val()
+            localStorage.setItem("form_name",formName)
+
+          });
+
+          var formName = localStorage.getItem("form_name");
+          if (formName != null && formName != "undefined") {
+             var about_parrafo = $('#about p');
+             about_parrafo.html("<strong>Bienveniodo: "+ formName + "</strong>")
+             about_parrafo.append("<a href='#' id='logout'>Cerrar Sesión</a>")
+
+             $('#login form').hide();
+
+             $('#logout').click(function(){
+               localStorage.clear();
+               location.reload();
+             });
+          }
+            //Acordeon
+            if (window.location.href.indexOf('about') > -1) {
+              $('#acordeon').accordion();
+            }
+
+            //Reloj
+            if (window.location.href.indexOf('reloj') > -1) {
+              setInterval(function(){
+                var reloj = moment().format('hh:mm:ss');
+                $('#reloj').html(reloj);
+              },1000);
+
+            }
+
+
+
       });
